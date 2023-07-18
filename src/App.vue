71ab7b2a17b0f5e381ac3ad3d67f8958c9ed198f
zh4188 <1746164155@qq.com>
2023-07-18 11:43:59 +0800

    引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-<router-view></router-view>
-<h1>黑马头条</h1>
-<div>
-  <i class="toutiao icon-yuedu"></i>
-</div>
+    <router-view></router-view>
+    <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao icon-yuedu"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
@@ -15,5 +20,4 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+// 引入Vant 核心库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
